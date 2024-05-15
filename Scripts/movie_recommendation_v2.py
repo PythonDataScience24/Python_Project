7e05963409f3b49dcbcf52feee387137735c82b8
riinajh <jhriina@gmail.com>
2024-05-15 13:15:58 +0200

    combining JR and MB scripts

--- Scripts/movie_recommendation_v2.py
+++ Scripts/movie_recommendation_v2.py
@@ -64,3 +64,85 @@ filtered_actors_df=df_actors[df_actors['nconst'].isin([actor.id for actor in act
 
 filtered_movies_df #use these dataframes to search for recommended movies with Marcine's algorithm
 filtered_actors_df
+
+def recommend_movies(preferences):
+    """
+    Function to recommend movies based on user preferences.
+    """
+    # Filter movies based on genres, actors and directors
+    genres = preferences[1]
+    actors = preferences[2]
+    directors = preferences[3]
+
+    # Filter movies based on genres
+    genre_movies = movies_df[movies_df['genres'].isin(genres)]
+
+    # Filter movies based on actors
+    preffered_actor_rows = actors_df[actors_df['primaryName'].isin(actors)]
+    # get the movies from column knownForTitles and split them into a list
+    preffered_movies = preffered_actor_rows['knownForTitles'].str.split(',').tolist()
+    # get all movies from movies_df that are in the list of preffered_movies
+    actor_movies = movies_df[movies_df['tconst'].isin([item for sublist in preffered_movies for item in sublist])]
+
+    # Filter movies based on directors
+    director_movies = directors_df[directors_df['primaryName'].isin(directors)]
+    # get the movies from column knownForTitles and split them into a list
+    director_movies = director_movies['knownForTitles'].str.split(',').tolist()
+    # get all movies from movies_df that are in the list of director_movies
+    director_movies = movies_df[movies_df['tconst'].isin([item for sublist in director_movies for item in sublist])]
+
+
+    # Combine filtered movies
+    recommended_movies = pd.concat([genre_movies, actor_movies, director_movies])
+
+    # Remove duplicates
+    recommended_movies = recommended_movies.drop_duplicates()
+
+    # # Sort movies by rating
+    # recommended_movies = recommended_movies.sort_values(by='rating', ascending=False)
+
+    # preferred_movies is dict with movie names and ratings
+    preferred_movies = preferences[0]
+    # get the movies from movies_df that are in the preferred_movies
+    # TODO or originalTitle?
+    rated_movies = movies_df[movies_df['primaryTitle'].isin(preferred_movies.keys())]
+    # add the ratings to the rated_movies
+    rated_movies['rating'] = rated_movies['primaryTitle'].map(preferred_movies)
+
+
+
+    ### Above part is good, below needs further work
+
+
+
+    # TODO split on knownForTitles and then merge with movies_df probably or implement by hand instead of using libs
+    # Merge additional information (like actors and directors) into recommended_movies so that each movie has corresponding attributes that can be used for content-based filtering.
+    recommended_movies = recommended_movies.merge(directors_df, on='knownForTitles', how='left')
+    recommended_movies = recommended_movies.merge(actors_df, on='knownForTitles', how='left')
+
+    # Convert genres to a list if it's a string of genres separated by commas
+    recommended_movies['genres'] = recommended_movies['genres'].apply(lambda x: x.split(','))
+
+    # One-hot encode genres
+    mlb = MultiLabelBinarizer()
+    genres_encoded = mlb.fit_transform(recommended_movies['genres'])
+
+    # Add new features back to df_B
+    recommended_movies = recommended_movies.join(pd.DataFrame(genres_encoded, columns=mlb.classes_, index=recommended_movies.index))
+
+    # Calculate similarity between all movies
+    similarity_matrix = cosine_similarity(genres_encoded)
+
+    # Only consider movies you've rated
+    rated_indices = rated_movies['tconst'].isin(recommended_movies['tconst'])
+    weighted_scores = similarity_matrix[rated_indices].T.dot(rated_movies['rating'])
+
+
+
+
+
+    # Return top 5 movies
+    return recommended_movies.head(5)
+
+user_preferences = input_preferences()
+recommend_movies(user_preferences)
